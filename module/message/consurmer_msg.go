269e4820727fe5ff3aca9c6f5bdeac40f5bd8d06
hh <hhh@gmail.com>
2025-09-04 17:04:40 +1000

    消息发送回执信息处理

--- module/message/consurmer_msg.go
+++ module/message/consurmer_msg.go
@@ -2,6 +2,7 @@ package message
 
 import (
 	pb "PProject/gen/message"
+	"PProject/service/chat"
 
 	msgcli "PProject/service/msg"
 	util "PProject/tools"
@@ -10,11 +11,12 @@ import (
 )
 
 func HandlerTopicMessage(topic string, key, value []byte) error {
-	msg, err := util.ParseFrameJSON(value)
+	msg, err := util.DecodeFrame(value)
 	if err != nil {
 		glog.Infof("topic key :%v Parse msg error: %s", topic, err)
 		return err
 	}
+	/// 写数据库
 
 	if msg.Type == pb.MessageFrameData_DATA {
 		//msg.WsRelayBound <- msg
@@ -24,7 +26,19 @@ func HandlerTopicMessage(topic string, key, value []byte) error {
 			return err
 		}
 
+		deliverMsg := chat.BuildSendSuccessAckDeliver(msg.From, msg.GetPayload().ClientMsgId, msg.GetPayload().ServerMsgId, msg)
+		deliverMsgData, err := util.EncodeFrame(deliverMsg)
+		if err != nil {
+			glog.Infof("BuildSendSuccessAckDeliver EncodeFrame topic key :%v Replay msg error: %s", topic, err)
+		}
+		err = msgcli.ReplayMsg(deliverMsgData)
+		if err != nil {
+			glog.Infof("BuildSendSuccessAckDeliver topic key :%v Replay msg error: %s", topic, err)
+		}
+
 	}
+	
+	// 下发给自己
 
 	//glog.Infof("[TestTopic] key=%s, value=%s", key, value)
 	return nil
--- tools/msg.go
+++ tools/msg.go
@@ -7,11 +7,21 @@ import (
 	"google.golang.org/protobuf/encoding/protojson"
 )
 
-func ParseFrameJSON(raw []byte) (*pb.MessageFrameData, error) {
-	frame := &pb.MessageFrameData{}
-	um := protojson.UnmarshalOptions{DiscardUnknown: true}
-	if err := um.Unmarshal(raw, frame); err != nil {
-		return nil, fmt.Errorf("unmarshal frame failed: %w", err)
+func EncodeFrame(req *pb.MessageFrameData) ([]byte, error) {
+	if req == nil {
+		return nil, fmt.Errorf("EncodeFrame: req is nil")
 	}
-	return frame, nil
+	return protojson.Marshal(req)
+}
+
+// DecodeFrame 从 []byte 解析回 MessageFrameData
+func DecodeFrame(data []byte) (*pb.MessageFrameData, error) {
+	if len(data) == 0 {
+		return nil, fmt.Errorf("DecodeFrame: data is empty")
+	}
+	var frame pb.MessageFrameData
+	if err := protojson.Unmarshal(data, &frame); err != nil {
+		return nil, err
+	}
+	return &frame, nil
 }
